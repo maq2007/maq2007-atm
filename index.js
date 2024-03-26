a3ab561b442853ffe58bb624e980ace43445e923
maq2007 <mayanqureshi6@gmail.com>
2024-03-27 00:47:53 +0500

    first commit to this repo

---
+++ index.js
@@ -0,0 +1,25 @@
+import inquirer from "inquirer";
+let myBalance = '10000'; // dollar
+console.log();
+let myPin = "12345";
+let pinAnswer = await inquirer.prompt([
+    {
+        message: "enter your pin",
+        type: "number",
+        name: "pin"
+    }
+]);
+if (pinAnswer === myPin) {
+    console.log('Correct Pin !!!');
+    let operationAns = await inquirer.prompt([
+        {
+            message: "What do you wanna do",
+            type: "list",
+            name: 'operations',
+            choices: ['withdraw', 'check balance'],
+        }
+    ]);
+}
+else {
+    console.log('Wrong Pin');
+}
