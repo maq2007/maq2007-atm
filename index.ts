e7c7c699725c83ebaa710721c331ba80622560b7
maq2007 <mayanqureshi6@gmail.com>
2024-03-28 22:08:02 +0500

    changed a string to number type

--- index.ts
+++ index.ts
@@ -1,30 +1,19 @@
 import inquirer from "inquirer";
 
-let myBalance = '10000';// dollar
-console.log()
-let myPin ="12345";
+let myBalance = "10000";// dollar
+console.log(myBalance);
+let myPin = 12345;
 
 let pinAnswer = await inquirer.prompt([
     {
         message: "enter your pin",
         type: "number",
-        name: "pin"
+        name: "pin",
     }
 ])
 
 if(pinAnswer === myPin){
-    console.log('Correct Pin !!!');
-
-    let operationAns = await inquirer.prompt([
-           {
-            message: "What do you wanna do",
-            type: "list",
-            name: 'operations',
-            choices: ['withdraw','check balance'],
-           }
-           
-    ])
+    console.log('Correct Pin !!');
 }
 else{
-    console.log('Wrong Pin');
 }
